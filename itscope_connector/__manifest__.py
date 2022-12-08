e2dc991e3a012a3b8618ca9728bb89a7c03ba81a
akhan <akhan@immap.org>
2022-12-08 09:17:55 +0500

    aleemcaan_05_itscope_connector[Version reverted]

--- itscope_connector/__manifest__.py
+++ itscope_connector/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '15.0.1',
+    'version': '15.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product', 'website_sale'],
