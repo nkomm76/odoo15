b0776e0d638319d26e7eeeda12a0343f6713f449
aleemcaan <aleemcaan@gmail.com>
2025-04-02 23:24:25 +0500

    [RM] - unused modules

--- itscope_connector/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import company
-from . import product
-from . import res_config_settings
--- itscope_connector/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    itscope_supplier_id = fields.Char(copy=False)
