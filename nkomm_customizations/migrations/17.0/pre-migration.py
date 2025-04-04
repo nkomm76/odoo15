b84870190ec280c43adf8be97bece8de88bf6306
aleemcaan <aleemcaan@gmail.com>
2025-04-04 16:07:15 +0500

    [FIX] - 003

--- nkomm_customizations/migrations/17.0/pre-migration.py
+++ nkomm_customizations/migrations/17.0/pre-migration.py
@@ -2,12 +2,10 @@ from odoo import SUPERUSER_ID
 from odoo.api import Environment
 
 def migrate(cr, version):
-    from odoo.api import Environment
-
     env = Environment(cr, SUPERUSER_ID, {})
 
-    module_name = 'beone_sub_draft_invoice'  # Replace with your target module
-
+    # Force-uninstall the unwanted module before Odoo 17 loads
+    module_name = 'beone_sub_draft_invoice'
     module = env['ir.module.module'].search([('name', '=', module_name)], limit=1)
-    if module and module.state == 'installed':
-        module.button_immediate_uninstall()
+    if module and module.state in ('installed', 'to upgrade'):
+        module.write({'state': 'uninstalled'})
